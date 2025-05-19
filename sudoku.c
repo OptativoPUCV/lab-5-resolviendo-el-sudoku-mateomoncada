a011c094c681af046de8e7b2d94bb4875b7b43c4
Your Name <you@example.com>
2025-05-18 21:46:53 -0500

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,6 +44,30 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
+  int seen[10];
+
+  for(int = 0 ; i < 9 ; i++){
+    for(int k = 0 ; k < 9 ; k++) seen[k] = 0;
+    for(int j = 0 ; j < 9 ; j++){
+      int valor = n->sudo[i][j];
+      if(valor == 0) continue;
+      if(seen[valor] == 1) return 0;
+      seen[valor] = 1;
+      }
+      
+    }
+
+    fir(int j = 0 ; j < 9 ; j++){
+      for (int k = 0 ; k < 9 ; k++) seen[k] = 0;
+      for (int i = 0 ; i < 9 ; i++){
+        int valor = n->sudo[i][j];
+        if(valor == 0) continue;
+        if(seen[valor] == 1) return 0;
+        seen[valor] = 1;
+      }
+      
+
+    
 
     return 1;
 }
